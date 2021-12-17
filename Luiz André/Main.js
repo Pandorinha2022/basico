9cf17259b435a4bcf75b9ba5b83df17287b463c4
Luiz André <luizandre12345@gmail.com>
2021-12-17 18:45:43 -0300

    Tarefa 1 - Luiz André

---
+++ Luiz André/Main.js
@@ -0,0 +1,14 @@
+var valor = 207.468;
+let nome = "Luiz";
+
+function mostrarValor(valor, nome) {
+    console.log("O valor enviado é:", valor, ", e o nome: ", nome)
+}
+
+mostrarValor(valor, nome);
+
+/*
+Comentário em bloco
+*/
+
+// Comentário em linha
