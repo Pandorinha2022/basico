9db29c4698f8a360c3d0ee5ee66f5a5fe8744907
davidoliveir4 <davidoliveir4@icloud.com>
2021-12-18 21:40:49 -0300

    Create Main.js
    - [x] 3) Copiar o arquivo Main.js da pasta Recursos/Dasafio2 para sua pasta e:
    
    - [x] 3.1 - Alterar o nome da função getValorPorIndex por qualquer outro.
    
    - [x] 3.2 - Passar mais de um parâmetro para função, seu nome. A função deve ser capaz de imprimir o valor e o seu nome.
    
    - [x] 3.3 - Usar a sintaxe de comentários. Um comentário em bloco e outro em linha.

---
+++ David Oliveira/Main.js
@@ -0,0 +1,14 @@
+/*
+function getValorPorIndex(valor){
+    console.log("O valor enviado é:", valor)
+}
+*/  
+
+function imprimirValor(nome, valor){
+console.log(`${nome} enviou o valor de ${valor}.`);
+}
+
+let nome = "David Oliveira"; //Variável nome
+let valor = 207.468; //Variável valor
+
+imprimirValor(nome, valor);//Chama a função
