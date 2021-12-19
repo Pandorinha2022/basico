b9b9fa08dcd5f45f11c416191bbca642f333da8b
educonejo <educonejomail@gmail.com>
2021-12-19 09:07:25 -0300

    Entrega da Tarefa 02

---
+++ Conrado Rosa/Main.js
@@ -0,0 +1,14 @@
+var valor = 207.468;
+let nome = "Conrado";
+
+function imprimirParametro(valor, nome){
+	console.log("O valor enviado é:", valor, ", e o nome: ", nome)
+}
+
+imprimirParametro(valor, nome);
+
+/*
+Comentário em bloco (Conrado esteve aqui)
+*/
+
+// Comentário em linha (Conrado esteve aqui)
