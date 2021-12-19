e37bee3fd5e86a2ddc45130e51c158996f1d5b32
ita0910 <itamar_perolim@hotmail.com>
2021-12-19 20:04:16 -0300

    Create Main.js

---
+++ Itamar Perolin/Main.js
@@ -0,0 +1,7 @@
+var valor = 207.468; // declara uma variavel (existem varios tipos de variaveis ex:Strings(texto puro), Numbers(numeros), Booleans(verdadeiro e falso))
+string = ItamarPerolin; 
+function MinhaFunçao(valor){
+console.log(nome, valor)
+}
+
+MinhaFunçao(valor);
