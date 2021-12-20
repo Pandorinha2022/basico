42dc4a7618b356b5daa48897f2a1735ade67332a
ita0910 <itamar_perolim@hotmail.com>
2021-12-19 21:19:51 -0300

    Update Main.js

--- Itamar Perolin/Main.js
+++ Itamar Perolin/Main.js
@@ -1,7 +1,17 @@
-var valor = 207.468; // declara uma variavel (existem varios tipos de variaveis ex:Strings(texto puro), Numbers(numeros), Booleans(verdadeiro e falso))
-string = ItamarPerolin; 
-function MinhaFunçao(valor){
-console.log(nome, valor)
-}
+var valor = 207.468;
+const string = 'The revolution will not be televised.';// declara uma constante
+const agente = 'Itamar Perolin'
 
-MinhaFunçao(valor);
+function frasedeefeito(){
+console.log('lema da revolução:', string)
+console.log("ano da revolução:", valor)
+console.log('prinsipal agente da revolução:', agente)
+
+}
+/*
+executa a função
+fim da tarefa
+*/
+
+
+frasedeefeito();
